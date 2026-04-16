bb5ca88fe60105fc491f3d9868b18d8a8969a40f
Livrikras <livrikras@icloud.com>
2026-04-16 13:45:39 +0200

    first commit

---
+++ js/movies-script.js
@@ -0,0 +1,119 @@
+"use script";
+
+const movies = [
+  {
+    id: 1,
+    title: "Inception",
+    genre: "science-fiction",
+    year: 2010,
+    duration: 2.28,
+    img: "img/inception.webp",
+    url: "https://www.imdb.com/title/tt1375666/",
+  },
+  {
+    id: 2,
+    title: "The Dark Knight",
+    genre: "action",
+    year: 2008,
+    duration: 2.32,
+    img: "img/the-dark-knight.webp",
+    url: "https://www.imdb.com/title/tt0468569/",
+  },
+  {
+    id: 3,
+    titel: "Forrest Gump",
+    genre: "drama",
+    year: 1994,
+    duration: 2.22,
+    img: "img/forrest-gump.webp",
+    url: "https://www.imdb.com/title/tt0109830/",
+  },
+  {
+    id: 4,
+    titel: "Superbad",
+    genre: "comedy",
+    year: 2007,
+    duration: 1.53,
+    img: "img/superbad.webp",
+    url: "https://www.imdb.com/title/tt0829482/",
+  },
+  {
+    id: 5,
+    titel: "It",
+    genre: "horror",
+    year: 2017,
+    duration: 2.15,
+    img: "img/it.webp",
+    url: "https://www.imdb.com/title/tt1396484/",
+  },
+  {
+    id: 6,
+    titel: "The Hangover",
+    genre: "comedy",
+    year: 2009,
+    duration: 1.4,
+    img: "img/the-hangover.webp",
+    url: "https://www.imdb.com/title/tt1119646/",
+  },
+  {
+    id: 7,
+    titel: "The Conjuring",
+    genre: "horror",
+    year: 2013,
+    duration: 1.52,
+    img: "img/the-conjuring.webp",
+    url: "https://www.imdb.com/title/tt1457767/",
+  },
+  {
+    id: 8,
+    titel: "Interstellar",
+    genre: "science-fiction",
+    year: 2014,
+    duration: 2.55,
+    img: "img/interstellar.jpg",
+    url: "https://www.imdb.com/title/tt0816692/",
+  },
+  {
+    id: 9,
+    titel: "The Matrix",
+    genre: "science-fiction",
+    year: 1999,
+    duration: 3.02,
+    img: "img/the-matrix.webp",
+    url: "https://www.imdb.com/title/tt0133093/",
+  },
+  {
+    id: 10,
+    titel: "Pulp Fiction",
+    genre: "drama",
+    year: 1994,
+    duration: 1.39,
+    img: "img/pulp-fiction.webp",
+    url: "https://www.imdb.com/title/tt0110912/",
+  },
+];
+
+const moviesContainer = document.querySelector("#movies-container");
+
+function displayMovies(movieList) {
+  moviesContainer.innerHTML = "";
+
+  movies.forEach((item) => {
+    moviesContainer.innerHTML += `
+        <div class="movie-card">
+            <article>
+                <h2>${item.titel}</h2>
+                <p>Genre: ${item.genre}</p>
+                <p>År: ${item.year}</p>
+                <p>Varighed: ${item.duration} timer</p>
+            </article>
+            <figure>
+                <a href="${item.url}" target="_blank" rel="noopener noreferrer"><img src="${item.img}" alt="${item.titel}"></a>
+                <figcaption>${item.titel}</figcaption>
+            </figure>
+        </div>
+        `;
+  });
+}
+
+displayMovies(movies);
